78dc947f388a0f4a0801b89218a57cd175ed0ad3
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 04:02:31 +0530

    Refactored: To combine Integer, Float and String to one generic class

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -2,55 +2,66 @@ package com.bridgelabz.maximumproblem;
 
 import java.util.Scanner;
 
-public class MaximumProblem {
-	
-	public static <E extends Comparable<E>> void findMaximum(E first, E second, E third) {
-		
-		E maximum = first;
-		
-		if(first.compareTo(second) == -1 ) {
-			if(second.compareTo(third) == -1) {
-				maximum = third;
+public class MaximumProblem<E extends Comparable<E>> {
+
+	E firstValue;
+	E secondValue;
+	E thirdValue;
+
+	public MaximumProblem(E firstValue, E secondValue, E thirdValue) {
+
+		this.firstValue = firstValue;
+		this.secondValue = secondValue;
+		this.thirdValue = thirdValue;
+	}
+
+	public void findMaximum() {
+
+		E maximum = firstValue;
+
+		if(firstValue.compareTo(secondValue) == -1 ) {
+			if(secondValue.compareTo(thirdValue) == -1) {
+				maximum = thirdValue;
 			}
 			else {
-				maximum = second;
+				maximum = secondValue;
 			}
 		}
-		else if(first.compareTo(third) == -1) {
-			maximum = third; 
+		else if(firstValue.compareTo(thirdValue) == -1) {
+			maximum = thirdValue; 
 		}
 		System.out.println("Maximum: "+maximum);
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("Enter First Integer: ");
 		Integer firstIntegerNumber = Integer.valueOf(scanner.nextInt());
 		System.out.print("Enter Second Integer: ");
 		Integer secondIntegerNumber = Integer.valueOf(scanner.nextInt());
 		System.out.print("Enter Third Integer: ");
 		Integer thirdIntegerNumber = Integer.valueOf(scanner.nextInt());
-		findMaximum(firstIntegerNumber, secondIntegerNumber, thirdIntegerNumber);
+		new MaximumProblem<Integer>(firstIntegerNumber, secondIntegerNumber, thirdIntegerNumber).findMaximum();
 		scanner.nextLine();
-	
+
 		System.out.print("Enter First Float: ");
 		Float firstFloatNumber = Float.valueOf(scanner.nextFloat());
 		System.out.print("Enter Second Float: ");
 		Float secondFloatNumber = Float.valueOf(scanner.nextFloat());
 		System.out.print("Enter Third Float: ");
 		Float thirdFloatNumber = Float.valueOf(scanner.nextFloat());
-		findMaximum(firstFloatNumber, secondFloatNumber, thirdFloatNumber);
+		new MaximumProblem<Float>(firstFloatNumber, secondFloatNumber, thirdFloatNumber).findMaximum();
 		scanner.nextLine();
-		
+
 		System.out.print("Enter First String: ");
 		String firstString = String.valueOf(scanner.nextLine());
 		System.out.print("Enter Second String: ");
 		String secondString = String.valueOf(scanner.nextLine());
 		System.out.print("Enter Third String: ");
 		String thirdString = String.valueOf(scanner.nextLine());
-		findMaximum(firstString, secondString, thirdString);
+		new MaximumProblem<String>(firstString, secondString, thirdString).findMaximum();
 		scanner.close();
 	}
 }
