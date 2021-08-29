cdba0d91d48dfaae769a76702f7f728a19585403
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 02:58:03 +0530

    Added: To find maximum between given three Integers

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,8 +1,37 @@
 package com.bridgelabz.maximumproblem;
 
-public class MaximumProblem {
+import java.util.Scanner;
 
+public class MaximumProblem {
+	
+	public static <E extends Comparable<E>> void findMaximum(E first, E second, E third) {
+		
+		E maximum = first;
+		
+		if(first.compareTo(second) == -1 ) {
+			if(second.compareTo(third) == -1) {
+				maximum = third;
+			}
+			else {
+				maximum = second;
+			}
+		}
+		else if(first.compareTo(third) == -1) {
+			maximum = third; 
+		}
+		System.out.println("Maximum: "+maximum);
+	}
+	
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter First Integer: ");
+		Integer firstIntegerNumber = Integer.valueOf(scanner.nextInt());
+		System.out.print("Enter Second Integer: ");
+		Integer secondIntegerNumber = Integer.valueOf(scanner.nextInt());
+		System.out.print("Enter Third Integer: ");
+		Integer thirdIntegerNumber = Integer.valueOf(scanner.nextInt());
+		findMaximum(firstIntegerNumber, secondIntegerNumber, thirdIntegerNumber);
+		scanner.close();
 	}
 }
